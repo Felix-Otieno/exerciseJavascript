c345ecfb4ed26b9086092be86e100f8668be7891
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-08-19 13:17:31 +0300

    first commit

---
+++ Quiz1.js
@@ -0,0 +1,14 @@
+function checkEvenOdd (number) {
+    
+
+    if (number % 2 === 0) {
+        console.log("even");
+    } else {
+        console.log("odd");
+    }
+
+} 
+checkEvenOdd("even");
+checkEvenOdd()
+
+
